45766bb1d6306fec6640add65c591234fb1741be
timour akhounov <timon1983@yandex.ru>
2021-12-01 15:23:31 +0500

    Start dev project

--- src/main/java/com/example/SpringBootKafkaSender/controller/MessageController.java
+++ src/main/java/com/example/SpringBootKafkaSender/controller/MessageController.java
@@ -1,27 +1,49 @@
 package com.example.SpringBootKafkaSender.controller;
 
-import com.example.SpringBootKafkaSender.model.Test;
-import org.springframework.http.ResponseEntity;
+import com.example.SpringBootKafkaSender.model.Message;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.File;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/create")
 public class MessageController {
 
+    private final static Logger log = LogManager.getLogger(MessageController.class);
+
     @GetMapping("")
-    public String getCreatePage(){
+    public String getCreatePage() {
         return "message-insert-form";
     }
 
+    /**
+     * Получение сообщения от клиента
+     * @param request
+     * @return
+     * @throws ServletException
+     * @throws IOException
+     */
     @PostMapping("")
-    public ResponseEntity<File> createMassage(Test test){
-        String path = test.getFile().getPath();
-        System.out.println(path);
-        return null;
+    public String createMassage(HttpServletRequest request) throws ServletException, IOException {
+        log.info("Receive message");
+        Message message = Message.builder()
+                .title(request.getParameter("title"))
+                .size(request.getPart("file").getSize())
+                .dateOfCreate(LocalDate.now())
+                .author(request.getParameter("author"))
+                .contentType(request.getPart("file").getContentType())
+                .build();
+
+        System.out.println();
+
+        return "message-insert-form";
     }
 }
---
+++ src/main/java/com/example/SpringBootKafkaSender/service/MessageService.java
@@ -0,0 +1,4 @@
+package com.example.SpringBootKafkaSender.service;
+
+public interface MessageService {
+}
