498d787adeebea6c794ece671ac0a6946e02aaba
timour akhounov <timon1983@yandex.ru>
2021-12-28 10:55:19 +0500

    correct ClientCacheServiceTest

--- uisbks/src/test/java/com/example/uisbks/service/ClientCacheServiceTest.java
+++ uisbks/src/test/java/com/example/uisbks/service/ClientCacheServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.uisbks.service;
 
 import com.example.uisbks.dtomodel.DTOMessage;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -21,28 +22,30 @@ class ClientCacheServiceTest {
         String fileName = "abc.txt";
 
         Files.createDirectories(Paths.get("test-files"));
-        Files.createFile(Paths.get(String.format("test-files/%s", fileName)));
+        Files.createFile(Path.of("test-files", fileName));
 
         assertTrue(clientCacheService.isCached(fileName, "test-files"));
 
-        Files.deleteIfExists(Path.of(String.format("test-files/%s", fileName)));
+        Files.deleteIfExists(Path.of("test-files", fileName));
         Files.deleteIfExists(Path.of("test-files"));
 
     }
 
     @Test
+    @DisplayName("gfd gf gf")
     void check_setCache_Should_Set_File_To_Cache() throws IOException {
         DTOMessage dtoMessage = new DTOMessage();
-        dtoMessage.setFileNameForS3("abc.txt");
+        String actualFn = "abc.txt";
+        dtoMessage.setFileNameForS3(actualFn);
         dtoMessage.setContent(new byte[]{1, 2, 3, 4});
-        clientCacheService.setCache(dtoMessage, "test-files/");
-        File file = new File(String.format("test-files/%s", dtoMessage.getFileNameForS3()));
+        clientCacheService.setCache(dtoMessage, "test-files");
+        Path path = Path.of("test-files", dtoMessage.getFileNameForS3());
 
-        assertTrue(file.exists());
-        assertEquals(file.getName(), "abc.txt");
+        assertTrue(Files.exists(path));
+        assertEquals(path.getFileName().toString(), actualFn);
 
-        Files.deleteIfExists(Path.of(String.format("test-files/%s", dtoMessage.getFileNameForS3())));
-        Files.deleteIfExists(Path.of("test-files"));
-
+        Files.deleteIfExists(path);
+        // todo
+        Files.deleteIfExists(path.getParent());
     }
 }
